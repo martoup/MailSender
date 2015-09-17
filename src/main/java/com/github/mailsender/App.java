cb90c5dc994959a38e486137d89621586685a2ab
Martin Nikolov <martoup@gmail.com>
2015-09-17 21:47:16 +0300

    My first commit

---
+++ src/main/java/com/github/mailsender/App.java
@@ -0,0 +1,13 @@
+package com.github.mailsender;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
